2fc4e47e538635020b52dc6348b42b1fc4b38da7
lijinhong11 <mcjrhugh@163.com>
2023-05-19 22:45:54 +0800

    update

--- src/main/java/org/lins/mmmjjkx/mixtools/objects/interfaces/MixTabExecutor.java
+++ src/main/java/org/lins/mmmjjkx/mixtools/objects/interfaces/MixTabExecutor.java
@@ -1,49 +1,5 @@
 package org.lins.mmmjjkx.mixtools.objects.interfaces;
 
-import org.bukkit.Bukkit;
-import org.bukkit.command.PluginCommand;
-import org.bukkit.command.TabCompleter;
-import org.bukkit.entity.Player;
-import org.lins.mmmjjkx.mixtools.MixTools;
-import org.lins.mmmjjkx.mixtools.objects.interfaces.MixCommandExecutor;
+import io.github.linsminecraftstudio.polymer.command.PolymerTabExecutor;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public interface MixTabExecutor extends MixCommandExecutor, TabCompleter {
-    @Override
-    default void register() {
-        String require = requirePlugin();
-        if (require == null){
-            require = "";
-        }
-        if (!require.isBlank()){
-            if (Bukkit.getPluginManager().isPluginEnabled(require)){
-                try {
-                    PluginCommand cmd = MixTools.INSTANCE.getCommand(name());
-                    cmd.setExecutor(this);
-                    cmd.setTabCompleter(this);
-                }catch (Exception e) {
-                    MixTools.INSTANCE.getLogger().warning("Failed to register command '"+name()+"' : "+e.getMessage());
-                }
-
-            }
-        }else {
-            try {
-                PluginCommand cmd = MixTools.INSTANCE.getCommand(name());
-                cmd.setExecutor(this);
-                cmd.setTabCompleter(this);
-            }catch (Exception e) {
-                MixTools.INSTANCE.getLogger().warning("Failed to register command '"+name()+"' : "+e.getMessage());
-            }
-        }
-    }
-
-    default List<String> getPlayerNames(){
-        List<String> list = new ArrayList<>();
-        for (Player p: Bukkit.getOnlinePlayers()){
-            list.add(p.getName());
-        }
-        return list;
-    }
-}
+public interface MixTabExecutor extends MixCommandExecutor, PolymerTabExecutor {}
